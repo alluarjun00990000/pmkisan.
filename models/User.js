53b179743eda12a8e4e73c374c598eb84d76a1cb
alluarjun00990000 <alluarjun00990000@gmail.com>
2025-02-13 12:03:57 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
     mobile: { type: String, required: true },
+    aadhaar: { type: String, required: true },
     dob: { type: String, required: true },
     pan: { type: String, required: true },
     uniqueid: { type: String, required: true, unique: true }
