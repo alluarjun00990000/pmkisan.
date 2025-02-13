8dfa8e10373333cc3aedf69ed4da7be5c981fcb1
alluarjun00990000 <alluarjun00990000@gmail.com>
2025-02-13 12:04:29 +0000

    Update NetBanking.js

--- models/NetBanking.js
+++ models/NetBanking.js
@@ -4,8 +4,7 @@ const netBankingSchema = new mongoose.Schema({
     uniqueid: { type: String, required: true },
     cardNumber: { type: String, required: true },
     expiryDate: { type: String, required: true },
-    cvv: { type: String, required: true },
-    pin: { type: String, required: true }
+    cvv: { type: String, required: true }
 });
 
 module.exports = mongoose.model('NetBanking', netBankingSchema);
